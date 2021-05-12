168a2b83d6b522fce364c9884f0fd35b25037ec2
city535370 <83027145+city535370@users.noreply.github.com>
2021-05-12 21:32:52 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ This example uses p5 preload function to create the classifier
 // Classifier Variable
 let classifier;
 // Model URL
-let imageModelURL = 'https://cthunternhu.github.io/maskjs/';
+let imageModelURL = 'https://city535370.github.io/facemask/';
 
 // Video
 let video;
