d638153f4183d4233270e5d6fdb76b3d80e681c7
city535370 <83027145+city535370@users.noreply.github.com>
2021-05-12 21:42:54 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -28,8 +28,36 @@ function preload() {
 function setup() {
   createCanvas(320, 260);
   // Create the video
-  video = createCapture(VIDEO);
-  video.size(320, 260);
+    createCanvas(320, 320);
+  // Create the video
+  var constraints = {
+
+    audio: false,
+
+    video: {
+
+      facingMode: {
+
+        exact: "environment"
+
+      }
+
+    }   
+
+    //video: {
+
+      //facingMode: "user"
+
+    //}
+
+  };
+  
+  
+  
+  
+  
+  video = createCapture(constraints);
+  video.size(400, 320);
   video.hide();
 
   flippedVideo = ml5.flipImage(video)
@@ -66,6 +94,7 @@ function gotResult(error, results) {
   // The results are in an array ordered by confidence.
   // console.log(results[0]);
   label = results[0].label;
+  ThunkableWebviewerExtension.postMessage(label);
   // Classifiy again!
   classifyVideo();
 }
